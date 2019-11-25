97e1a61068d3c6ab773fb17d31d7761ad139c36b
weetgeen <hans-pot@hotmail.com>
2019-11-25 22:36:17 +0100

    Finished 1st week of exercies

--- Assignment/exercise_rectangle.py
+++ Assignment/exercise_rectangle.py
@@ -4,13 +4,21 @@ class Rectangle:
     """A class to manufacture rectangle objects"""
 
     def __init__(self, posn, width, height):
-        """Injtialize rectangle at posn, with width and height as specified"""
+        """Initialize rectangle at posn, with width and height as specified"""
         self.corner = posn
         self.width = width
         self.height = height
 
     def __str__(self):
         return "({0}, {1}, {2})".format(self.corner, self.width , self.height)
+
+    def area(self):
+        """Returns the area of a rectangle"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Returns the perimeter of a rectangle"""
+        return 2 * self.height + 2 * self.width
 
 
 box = Rectangle(exercise_point.Point(0,0) , 100, 200)
